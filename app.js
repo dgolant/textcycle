4b14e837bd14f0528198381357fbeabcd0a72874
Dan Golant <dgolant@gmail.com>
2016-04-03 15:15:04 -0400

    itsssss allliiiive

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var request = require('request');
-var stationStatusMethods = require('./routes/stationstatus.js');
+var fs = require('fs');
+
 
 
 var tokens = require('./private/tokens.json');
 var settings = require('./private/settings.json');
+var stationMetaDataFromDisk = require('./stationMetaData.json');
 var twilio = require('twilio'),
     client = twilio(tokens.accountSID, tokens.authToken),
     cronJob = require('cron').CronJob;
@@ -20,6 +22,7 @@ var twilio = require('twilio'),
 var routes = require('./routes/index');
 var stations = require('./routes/stations');
 var stationstatus = require('./routes/stationstatus');
+var DEBUG = false;
 
 var app = express();
 
@@ -70,15 +73,30 @@ app.use(function(err, req, res, next) {
     });
 });
 
+//CRONs
 
-
-var textJob = new cronJob("0-59 0-23 * * *", function() {
+var textJob = new cronJob("45 7 * * *", function() {
+    console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+    console.log('textJob STARTED');
     getStationStatus(getTrackedStationInformation);
 }, null, true);
 
 
+var stationInformationJob = new cronJob("0 0 * * *", function() {
+    console.log('//////////////////////////////');
+    console.log('stationInformationJob STARTED');
+    getStationMetaData();
+}, null, true);
 
-function getStationStatus(callback) {
+
+
+
+
+
+
+
+
+function getStationStatus(trackedStationCallback) {
     url = 'https://gbfs.bcycle.com/bcycle_pacersbikeshare/station_status.json';
 
     request(url, function(error, response, html) {
@@ -87,24 +105,41 @@ function getStationStatus(callback) {
         if (!error) {
             var stationStatusObject = JSON.parse(html);
             //callback will be the settings check
-            callback(stationStatusObject.data);
+            trackedStationCallback(stationStatusObject.data);
         } else {
             console.log('error, broham:' + error);
         }
     })
 }
 
-function sendMessage(Message) {
-    console.log('-------------MESSAGE FIRED!----------------');
-    client.sendMessage({ to: tokens.receivingNumber, from: tokens.twilioNumber, body: 'Hello! Hope you’re having a good day!' }, function(err, data) {
-        if (!err) {
-            console.log('success!');
-        } else {
-            console.log(err);
-        }
 
-    });
+//StationMetaData Methods
+
+function getStationMetaData() {
+    url = 'https://gbfs.bcycle.com/bcycle_pacersbikeshare/station_information.json';
+
+    request(url, function(error, response, html) {
+
+        // First we'll check to make sure no errors occurred when making the request
+        if (!error) {
+            var stationMetaData = JSON.parse(html);
+            //callback will be the settings check
+            writeStationMetaDataToDisk(stationMetaData);
+        } else {
+            console.log('error, broham:' + error);
+        }
+    })
 }
+
+function writeStationMetaDataToDisk(stationData) {
+    fs.writeFile('stationMetaData.json', JSON.stringify(stationData), function(err) {
+        if (err) return console.log(err);
+        //console.log(JSON.stringify(stationData) + ' > stationMetadata.json');
+    });
+};
+
+
+//Messaging methods
 
 function getTrackedStationInformation(stationStatusObject) {
     var trackedStationInformation = [];
@@ -112,29 +147,44 @@ function getTrackedStationInformation(stationStatusObject) {
         settings.trackedStations.forEach(function(trackedStation, i) {
             if (station['station_id'] == trackedStation) {
                 trackedStationInformation.push(station);
-                // console.log('++++++++++++++++++++++++++++++++++++++++++++');
-                // console.log('on the fly raw object: ' + trackedStationInformation)
             }
         });
     });
-
-    console.log('---------------------------------------------------');
-    console.log('rawMessageObject:' + trackedStationInformation);
-
     buildMessageArray(trackedStationInformation);
-
 }
 
-function buildMessageArray(stationStatusInformation){
-        var textMessageArray = [];
-        stationStatusInformation.forEach(function(station, i){
-            var messageString="Currently at Station: "+station['station_id']+" there are "+station['num_bikes_available']+" available bicycles and "+station['num_docks_available']+" docks available.";
-            textMessageArray.push(messageString);
+function buildMessageArray(stationStatusInformation) {
+    var textMessageArray = [];
+    //    console.log('**********************************')
+    var stationsMDArray = stationMetaDataFromDisk['data']['stations'];
+    stationStatusInformation.forEach(function(station, i) {
+        stationsMDArray.forEach(function(stationMetaObject, i) {
+            if (station['station_id'] == stationMetaObject['station_id']) {
+                var messageString = "There are currently "  + station['num_bikes_available'] + " bicycles available and " + station['num_docks_available'] + " open docks at the "+ stationMetaObject['name']+" Bikeshare Station";
+                textMessageArray.push(messageString);
+            }
+
         });
 
-        textMessageArray.forEach(function(message, i){
-            sendMessage(message);
-        });
+    });
 
+    textMessageArray.forEach(function(message, i) {
+        sendMessage(message);
+    });
+}
+
+
+function sendMessage(textMessage) {
+    if (!DEBUG) {
+        console.log('-------------MESSAGE FIRED!----------------');
+        client.sendMessage({ to: tokens.receivingNumber, from: tokens.twilioNumber, body: textMessage }, function(err, data) {
+            if (!err) {
+                console.log('success!');
+            } else {
+                console.log(err);
+            }
+
+        });
+    }
 }
 module.exports = app;
