0b5c1d04bb89fff3394d8d7f2143c07175afde7c
Dan Golant <dgolant@gmail.com>
2016-04-17 19:31:18 -0400

    testing json parse

--- routes/SMSReceiver.js
+++ routes/SMSReceiver.js
@@ -10,12 +10,12 @@ var jsonParser = bodyParser.json();
 
 router.post('/',jsonParser, function(req, res) {
 	if (!req.body) {
-		return res.sendStatus(400);
+		return res.sendStatus(500);
 		console.log('BAD JSON FORM REQUEST');
 	}
 	var smsPayload = req.body;
 	console.log(smsPayload.Body);
-	var stationID = stationChooser.stationIDForName('StationID: '+smsPayload.Body);
+	//var stationID = stationChooser.stationIDForName('StationID: '+smsPayload.Body);
 	res.send('welcome, ' + req.body);
 });
 
