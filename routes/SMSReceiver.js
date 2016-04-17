f652751968bedddf9b5c2c2cca777b522df79a74
Dan Golant <dgolant@gmail.com>
2016-04-17 15:36:52 -0400

    uncommented route

--- routes/SMSReceiver.js
+++ routes/SMSReceiver.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 var util = require('util');
 
-app.post('/', function(req, res) {
+router.post('/', function(req, res) {
 	console.log(req);
     res.send('Got a POST request');
 });
