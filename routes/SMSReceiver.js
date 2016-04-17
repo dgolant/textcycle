beeb01889da9a0ac4e912804295086a58667d0b5
Dan Golant <dgolant@gmail.com>
2016-04-17 14:40:31 -0400

    why no work

--- routes/SMSReceiver.js
+++ routes/SMSReceiver.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
-var util = require('util');
+// var express = require('express');
+// var router = express.Router();
+// var request = require('request');
+// var util = require('util');
 
-app.post('/SMSReceiver', function (req, res) {
-  res.send('Got a POST request');
-});
+// app.post('/SMSReceiver', function (req, res) {
+//   res.send('Got a POST request');
+// });
