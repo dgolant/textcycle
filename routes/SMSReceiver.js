5bdf2c1038a884c0b8d3db54228c27b8743c601b
Dan Golant <dgolant@gmail.com>
2016-04-17 20:15:33 -0400

    beginnings of twiml response

--- routes/SMSReceiver.js
+++ routes/SMSReceiver.js
@@ -8,22 +8,22 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var twilio = require('twilio');
 
-router.post('/',jsonParser, function(req, res) {
-	// if (!req.body) {
-	// 	return res.sendStatus(500);
-	// 	console.log('BAD JSON FORM REQUEST');
-	// }
-	var smsPayload = req.body;
-	console.log(smsPayload.Body);
-	var stationID = stationChooser.stationIDForName(smsPayload.Body);
+router.post('/', jsonParser, function(req, res) {
+    // if (!req.body) {
+    // 	return res.sendStatus(500);
+    // 	console.log('BAD JSON FORM REQUEST');
+    // }
+    var smsPayload = req.body;
+    console.log(smsPayload.Body);
+    var stationID = stationChooser.stationIDForName(smsPayload.Body);
 
-	var twimlResp = new twilio.TwimlResponse();
-	twimlResp.message(stationID);
-	res.writeHead(200, {
-        'Content-Type':'text/xml'
-    });
-    console.log("stationID at twimlRESP:"+stationID);
-	res.send(twimlResp);
+    var twimlResp = new twilio.TwimlResponse();
+    twimlResp.message(stationID);
+    // res.writeHead(200, {
+    //        'Content-Type':'text/xml'
+    //    });
+    console.log("stationID at twimlRESP:" + stationID);
+    res.send(twimlResp);
 });
 
 module.exports = router;
